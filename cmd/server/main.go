dc09e94149f0b4dc4212557894d67337709dc326
Mark.mphw <70524457+Mark-mphw@users.noreply.github.com>
2025-11-26 22:23:07 +0700

    Edit main mod sum

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/phuvarth-wang/mini-ecommerce-backend/internal/database"
 	"github.com/phuvarth-wang/mini-ecommerce-backend/internal/routes"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -15,12 +15,15 @@ func main() {
 	db := database.Connect()
 
 	r := gin.Default()
-	r.SetTrustedProxies([]string{"127.0.0.1"})
 
-	supabaseURL := os.Getenv("SUPABASE_URL")
-	if supabaseURL == "" {
-		log.Fatal("Missing SUPABASE_URL in .env")
-	}
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+	r.SetTrustedProxies(nil)
 
 	log.Println("Using Supabase HS256 Auth Mode")
 	routes.RegisterRoutes(r, db)
